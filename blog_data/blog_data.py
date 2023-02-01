690f4cdfbf24e12e0cf433af2843c69536030190
claytoncook12 <claytoncook12@gmail.com>
2023-02-01 14:10:52 -0500

    updating stypes for tag

--- blog_data/blog_data.py
+++ blog_data/blog_data.py
@@ -5,7 +5,7 @@ blog_posts: list[dict] = [
         "short_desc": "Setting up shared custom model validation through an angular service.",
         "main_image": "clem-onojeghuo--YMhg0KYgVc-unsplash.jpg",
         "photo_credit": 'Photo by <a href="https://unsplash.com/@clemono?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Clem Onojeghuo</a> on <a href="https://unsplash.com/s/photos/nature-free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>',
-        "tags": ["programming", "angular", "testing"],
+        "tags": ["Programming", "Angular", "Testing"],
         "html_file": "shared-custom-angular-form-validation-through-a-service.html"
     },
     {
@@ -14,7 +14,7 @@ blog_posts: list[dict] = [
         "short_desc": "Quick reference for command line commands I use for setting up a python project.",
         "main_image": "luca-bravo-lWAOc0UuJ-A-unsplash.jpg",
         "photo_credit": 'Photo by <a href="https://unsplash.com/@lucabravo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Luca Bravo</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>',
-        "tags": ["programming", "python"],
+        "tags": ["Programming", "Python"],
         "html_file": "setting-up-a-python-project.html"
     },
 ]
