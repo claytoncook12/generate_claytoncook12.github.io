c1a0f4bf12314320efcf2fd6accdc808a2de5b85
claytoncook12 <claytoncook12@gmail.com>
2023-04-13 15:08:43 -0400

    renamed files

--- html_projects/image-crop-and-resize/js/script.js
+++ html_projects/image-crop-and-resize/js/script.js
@@ -1,52 +1,178 @@
 const fileInput = document.querySelector(".file-input"),
-filterOptions = document.querySelectorAll(".filter button"),
-// filterName = document.querySelector(".filter-info .name"),
-// filterValue = document.querySelector(".filter-info .value"),
-// filterSlider = document.querySelector(".slider input"),
-// rotateOptions = document.querySelectorAll(".rotate button"),
-previewImg = document.querySelector(".preview-img img"),
-resetFilterBtn = document.querySelector(".reset-filter"),
 chooseImgBtn = document.querySelector(".choose-img"),
-saveImgBtn = document.querySelector(".save-img");
+addImgSvg = document.querySelector("#temp-image"),
+imageContainer = document.querySelector(".image-container"),
+workingImage = document.getElementById("working-image"),
+overlay = document.querySelector(".overlay"),
+cropX = document.getElementById("cropX"),
+cropXLength = document.getElementById("cropXLength"),
+cropY = document.getElementById("cropY"),
+cropYLength = document.getElementById("cropYLength");
 
-let cropX = "", cropY = "", cropXLength = "", cropYLength = "", scale = "0";
+var workingImageHeight = 0,
+workingImageWidth = 0;
+
+const updateWorkingImage = () => {
+    workingImageHeight = workingImage.getBoundingClientRect().height;
+    workingImageWidth = workingImage.getBoundingClientRect().width;
+    defaultOverlayLocation();
+    scaleOverlay();
+    setImageCropParameters();
+    hideAddImgSvg();
+}
+
+const defaultOverlayLocation = () => {
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+}
+
+const scaleOverlay = () => {
+    // Set Overlay to Image Size
+    overlay.style.height = `${workingImageHeight}px`;
+    overlay.style.width = `${workingImageWidth}px`;
+    overlay.removeAttribute("hidden");
+}
+
+const setImageCropParameters = () => {
+    cropX.value = 0;
+    cropX.max = Math.round(workingImageWidth);
+    cropXLength.value = Math.round(workingImageWidth);
+    cropXLength.max = Math.round(workingImageWidth);
+    
+    cropY.value = 0;
+    cropY.max = Math.round(workingImageHeight);
+    cropYLength.value = Math.round(workingImageHeight);
+    cropYLength.max = Math.round(workingImageHeight);
+}
+
+const hideAddImgSvg = () => {
+    addImgSvg.hidden = true;
+}
+
+const cropOutsideWidthBound = () => {
+    return ((parseInt(cropX.value) + parseInt(cropXLength.value)) > workingImageWidth)
+}
+
+const cropOutsideHeightBound = () => {
+    return ((parseInt(cropY.value) + parseInt(cropYLength.value)) > workingImageHeight)
+}
+
+cropY.addEventListener("change", (event) => {
+    if (cropOutsideHeightBound()) {
+        // Update Overlay Height
+        overlay.style.top = `${event.target.value}px`;
+        const newOverlayHeight = workingImageHeight - event.target.value;
+        overlay.style.height = `${newOverlayHeight}px`;
+        // Update Crop Y Length
+        cropYLength.value = Math.round(workingImageHeight - event.target.value);
+    } else {
+        // Update Overlay Location
+        overlay.style.top = `${event.target.value}px`;
+    }
+})
+
+cropX.addEventListener("change", (event) => {
+    if (cropOutsideWidthBound()) {
+        // Update Overlay Width
+        overlay.style.left = `${event.target.value}px`;
+        const newOverlayWidth = workingImageWidth - event.target.value;
+        overlay.style.width = `${newOverlayWidth}px`;
+        // Update Crop X Length
+        cropXLength.value = Math.round(workingImageWidth - event.target.value);
+    } else {
+        // Update Overlay Location
+        overlay.style.left = `${event.target.value}px`;
+    }
+})
+
+cropXLength.addEventListener("change", (event) => {
+    if (parseInt(cropX.value) > 0) {
+        if (cropOutsideWidthBound()) {
+            // Update Form Values
+            cropXLength.max = cropX.max - parseInt(cropX.value);
+            cropXLength.value = (cropX.max - parseInt(cropX.value)).toString();
+            showSnackbar("Crop Length Outside Picture Bounds.");
+        }
+        // Update Overlay
+        overlay.style.width = `${cropXLength.value}px`;
+    } else {
+    overlay.style.width = `${event.target.value}px`;
+    }
+})
+
+cropYLength.addEventListener("change", (event) => {
+    if (parseInt(cropY.value) > 0) {
+        if (cropOutsideHeightBound()) {
+            // Update Form Values
+            cropYLength.max = cropY.max - parseInt(cropY.value);
+            cropYLength.value = (cropY.max - parseInt(cropY.value)).toString();
+            showSnackbar("Crop Height Outside Picture Bounds.");
+        }
+        // Update Overlay
+        overlay.style.height = `${cropYLength.value}px`;
+    } else {
+    overlay.style.height = `${event.target.value}px`;
+    }
+})
+
+const reset = () => {
+    defaultOverlayLocation();
+    scaleOverlay();
+    setImageCropParameters();
+}
 
 const loadImage = () => {
     let file = fileInput.files[0];
     if(!file) return;
-    previewImg.src = URL.createObjectURL(file);
-    previewImg.addEventListener("load", () => {
-        resetFilterBtn.click();
-        document.querySelector(".container").classList.remove("disable");
+    workingImage.src = URL.createObjectURL(file);
+    workingImage.addEventListener("load", () => {
+        imageContainer.removeAttribute("hidden");
+        updateWorkingImage();
+        imageContainer.style.height = overlay.style.height;
     });
 }
 
-const resetFilter = () => {
-    filterOptions[0].click();
-}
+const saveCropImage = () => {
+    console.log("Saving Crop Image File.");
 
-const saveImage = () => {
+    // Get Source Image Scale
+    scaleFactor =  workingImage.naturalWidth / workingImage.clientWidth;
+    
+    // Source Image Properties
+    sx = parseInt(cropX.value * scaleFactor);
+    sy = parseInt(cropY.value * scaleFactor);
+    sWidth = parseInt(cropXLength.value * scaleFactor);
+    sHeight = parseInt(cropYLength.value * scaleFactor);
+
+    // Destination Canvas
+    dx = 0;
+    dy = 0;
+    dWidth = parseInt(cropXLength.value);
+    dHeight = parseInt(cropYLength.value);
+    
+    // Create New Image
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
-    canvas.width = previewImg.naturalWidth;
-    canvas.height = previewImg.naturalHeight;
+    canvas.width = dWidth;
+    canvas.height = dHeight;
+    ctx.drawImage(workingImage, sx, sy, sWidth, sHeight,
+        dx, dy, dWidth, dHeight);
     
-    ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    if(rotate !== 0) {
-        ctx.rotate(rotate * Math.PI / 180);
-    }
-    ctx.scale(flipHorizontal, flipVertical);
-    ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-    
+    // Down Load New Image
     const link = document.createElement("a");
     link.download = "image.jpg";
     link.href = canvas.toDataURL();
     link.click();
 }
 
-// filterSlider.addEventListener("input", updateFilter);
-resetFilterBtn.addEventListener("click", resetFilter);
-saveImgBtn.addEventListener("click", saveImage);
+// Snack Bar Functions
+const showSnackbar = (textString) => {
+    const snackbar = document.getElementById("snackbar");
+    snackbar.innerHTML = textString;
+    snackbar.className = "show";
+    setTimeout(function(){ snackbar.className = snackbar.className.replace("show",""); },3000);
+}
+
 fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
+addImgSvg.addEventListener("click", () => fileInput.click());
