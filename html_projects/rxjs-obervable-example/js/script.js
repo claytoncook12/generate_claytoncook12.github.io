d2887d6fe3af134c0163a0308e7149ce490a8f28
claytoncook12 <claytoncook12@gmail.com>
2023-06-13 11:45:20 -0400

    working observable example

--- html_projects/rxjs-obervable-example/js/script.js
+++ html_projects/rxjs-obervable-example/js/script.js
@@ -1,34 +1,34 @@
-throw new Error(`Need to have a function that gets triggered on btn
-click and then subscribe to ajax request inside of that
-btn click function.`)
 
 const dataHolder = document.getElementById("data");
 const userID = document.getElementById("userId");
 const btn = document.getElementById("loadUsers");
 
-function addDataElement(text) {
+function addDataElement(id, email, name, image) {
   const dataDiv = document.createElement("div");
-  dataDiv.innerText = text;
+  dataDiv.classList.add("users")
+  dataDiv.innerHTML += `
+    <div class="left">
+        <img src="${image}" alt="User ${id} avatar image">
+    </div>
+    <div class="right">
+        <div class="rightText">Id: ${id}<br>${name}<br>${email}<br>
+        </div>
+    </div>`;
   dataHolder.appendChild(dataDiv);
 }
 
-const click = rxjs.fromEvent(btn, "click");
-
-click
-    .pipe(
-        rxjs.switchMap(
-            () => rxjs.ajax.ajax.getJSON(`https://reqres.in/api/users/${userID.value}`)
-            .pipe(
-                rxjs.operator.map(response => {
-                    //addDataElement(`User Id: ${response.data.id} and email ${response.data.email}`)
-                    console.log(response)
-                    }
-                )
-            )
-        )
-    )
+function makeRequest() {
+    rxjs.ajax.ajax.getJSON(`https://reqres.in/api/users/${userID.value}`)
     .subscribe({
-    next: (resp) => {console.log("To Next!")},
-    error: (error) => console.log(error),
-    complete: () => console.log("Complete!"),
+        next: (response) => {
+            addDataElement(
+                response.data.id,
+                response.data.email,
+                `${response.data.first_name} ${response.data.last_name}`,
+                response.data.avatar
+            )
+        },
+        error: (error) => window.alert('Users Lookup Failed'),
+        complete: () => console.log("Complete!"),
     });
+}
