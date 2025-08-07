ca1dc9323d0db75440a2bf7dc65b0eddf88ce478
Clayton Cook <claytoncook12@gmail.com>
2025-08-07 13:30:56 -0400

    add fall class signup

---
+++ html_projects/fall-2025-music-class/static/js/api.js
@@ -0,0 +1,39 @@
+async function submitFormData() {
+    const url = 'https://script.google.com/macros/s/AKfycbwg9mwD0pDbZiq4fNSydHN4BsyoO6u5CcG-SrUxxXaJj2hGppzPjwyjfZY6BmjE2awl/exec';
+    const formData = new URLSearchParams();
+    const dimmer = document.getElementById('dimmer');
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('message', message);
+
+    try {
+        dimmer.style.visibility = "visible";
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData
+        });
+
+        if (!response.ok) {
+            dimmer.style.visibility = "hidden";
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log('Success:', data);
+        formSuccess();
+        dimmer.style.visibility = "hidden";
+    } catch (error) {
+        console.error('Error:', error);
+        dimmer.style.visibility = "hidden";
+    }
+}
+
+function formSuccess() {
+    alert('Form Submitted Successfully!');
+    document.getElementById('myForm').reset();
+    document.getElementById('contact-success').style.visibility = "visible";
+    document.getElementById('all-content').hidden = true;
+}
